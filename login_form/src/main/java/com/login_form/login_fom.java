b0545df12de90e6b4aa16ca9f14ae7986c4fff5e
VIRAK33 <seamvirak.itc@gmail.com>
2019-09-17 16:13:04 +0700

    Initial commit

---
+++ login_form/src/main/java/com/login_form/login_fom.java
@@ -0,0 +1,16 @@
+package com.login_form;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.selectmenu.R;
+
+public class login_fom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
